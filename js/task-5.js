14604b08a904a8ed89934fd3b7f142df0e8ed7b5
KhrystynaKolomiiets <kaulitz89996@gmail.com>
2025-05-17 14:25:11 +0200

    fix task 2 and 5

--- js/task-5.js
+++ js/task-5.js
@@ -4,8 +4,9 @@ const buttonColor = document.querySelector(".change-color");
 buttonColor.addEventListener("click", changeMainColor);
 
 function changeMainColor() {
-  mainColor.textContent = getRandomHexColor();
-  document.body.style.backgroundColor = getRandomHexColor();
+  const newColor = getRandomHexColor();
+  mainColor.textContent = newColor;
+  document.body.style.backgroundColor = newColor;
 }
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
